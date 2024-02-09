d39d6e9fb63e9e2344f0aa4a3fbfa40d0516dc05
Elshan Mamedov <elikmtl@gmail.com>
2024-02-08 19:44:35 -0500

    initial commit

---
+++ Engineer.java
@@ -0,0 +1,29 @@
+package myPack;
+
+public class Engineer {
+
+    private String name;
+    private String position;
+
+    public Engineer(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+//getterst and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
